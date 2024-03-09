b98d89326d1f596e5efd5083ebee62047f867abf
amrayman100 <amrayman1996@yahoo.com>
2024-03-10 00:39:56 +0200

    Update page.tsx

--- app/proposal/page.tsx
+++ app/proposal/page.tsx
@@ -2,7 +2,7 @@ import { Proposal } from "@/components/product/proposal/proposal";
 
 export default async function ProposalPage() {
   return (
-    <main className="flex min-h-screen w-screen flex-col items-center justify-between lg:p-24 md:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 md:p-24">
       <div className="z-10 w-full items-center justify-between  text-sm lg:flex">
         <Proposal />
       </div>
