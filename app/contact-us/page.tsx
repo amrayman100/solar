18789925f1294c4577e40e861140fa777fb0ddc5
amrayman100 <amrayman1996@yahoo.com>
2024-08-24 01:29:22 +0300

    Update page.tsx

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -9,7 +9,7 @@ export default async function ContactUs() {
       <main className="flex-grow">
         <Header />
         <div className="flex mx-auto align-center justify-center mt-10">
-          <div className="m-10 lg:w-1/3 lg:mt-5 md:mt-5 p-10">
+          <div className="mg-5 lg:m-10 lg:w-1/3 lg:mt-5 md:mt-5 p-10">
             <ContactForm />
           </div>
           <Image
